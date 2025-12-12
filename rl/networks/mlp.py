caa4c5cd60337af2eeea4b3fc7267afd48295089
madyhr <marcusdyhr@gmail.com>
2025-12-12 11:35:54 +0100

    Added layer init from 37 PPO implementation details blog

--- rl/networks/mlp.py
+++ rl/networks/mlp.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -28,11 +29,11 @@ class MLP(nn.Module):
         layers = []
 
         for hidden_dim in self.hidden_size:
-            layers.append(nn.Linear(current_dim, hidden_dim))
+            layers.append(layer_init(nn.Linear(current_dim, hidden_dim)))
             layers.append(self.activation_func_class())
             current_dim = hidden_dim
 
-        layers.append(nn.Linear(current_dim, self.output_dim))
+        layers.append(layer_init(nn.Linear(current_dim, self.output_dim)))
 
         self.network = nn.Sequential(*layers)
 
@@ -49,3 +50,10 @@ class MLP(nn.Module):
 
         if any(h <= 0 for h in self.hidden_size):
             raise ValueError("Hidden dims must be positive.")
+
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    # Adapted from https://iclr-blog-track.github.io/2022/03/25/ppo-implementation-details/
+    nn.init.orthogonal_(layer.weight, std)
+    nn.init.constant_(layer.bias, bias_const)
+    return layer

c82bd1e3a94f1900a291e124ed9139fb2545e6f8
madyhr <marcusdyhr@gmail.com>
2026-01-05 15:55:45 +0100

    Refactored distance rewards to instead use velocity towards player

--- src/rewards.cpp
+++ src/rewards.cpp
@@ -8,38 +8,22 @@ namespace rl2 {
 
 // This function is used to define all the reward terms.
 void RewardManager::RegisterRewardTerms() {
+  AddTerm(
+      "velocity_towards_player", 1.0f,
+      [](const Scene& scene) -> std::array<float, kNumEnemies> {
+        std::array<float, kNumEnemies> value_array;
 
-  AddTerm("distance_progress", 1.0f,
-          [](const Scene& scene) -> std::array<float, kNumEnemies> {
-            std::array<float, kNumEnemies> value_array;
+        for (int i = 0; i < kNumEnemies; ++i) {
 
-            for (int i = 0; i < kNumEnemies; ++i) {
-              float prev_distance_to_player =
-                  (scene.player.prev_position_ - scene.enemy.prev_position[i])
-                      .Norm();
-              float distance_to_player =
-                  (scene.player.position_ - scene.enemy.position[i]).Norm();
-              value_array[i] = prev_distance_to_player - distance_to_player;
-            }
+          Vector2D dir_to_player =
+              (scene.player.position_ - scene.enemy.position[i]).Normalized();
+          float velocity_towards_player =
+              scene.enemy.velocity[i].Dot(dir_to_player);
+          value_array[i] = velocity_towards_player;
+        }
 
-            return value_array;
-          });
-
-  AddTerm("distance_to_player_exp", 1.0f,
-          [](const Scene& scene) -> std::array<float, kNumEnemies> {
-            std::array<float, kNumEnemies> value_array;
-
-            for (int i = 0; i < kNumEnemies; ++i) {
-              float distance_to_player =
-                  (scene.player.position_ - scene.enemy.position[i]).Norm();
-              // 0.005 is the inv scale of the exponential kernel. It is defined
-              // s.t. it has a value of 1/e at 200 units of distance. Used to
-              // incentivize being close to the player.
-              value_array[i] = std::exp(-distance_to_player * 0.005);
-            }
-
-            return value_array;
-          });
+        return value_array;
+      });
 
   AddTerm("was_terminated", -1000.0f,
           [](const Scene& scene) -> std::array<float, kNumEnemies> {

8feb75d8b70046696af83d00ed9130f6b772bb3e
madyhr <marcusdyhr@gmail.com>
2026-01-06 12:04:29 +0100

    Updated velocity to player reward to only be active when player is detected by ray hits

--- src/rewards.cpp
+++ src/rewards.cpp
@@ -1,6 +1,7 @@
 // src/rewards.cpp
 #include <array>
 #include "constants.h"
+#include "ray_caster.h"
 #include "reward_manager.h"
 #include "scene.h"
 
@@ -13,13 +14,20 @@ void RewardManager::RegisterRewardTerms() {
       [](const Scene& scene) -> std::array<float, kNumEnemies> {
         std::array<float, kNumEnemies> value_array;
 
+        int current_history_idx =
+            (scene.enemy.ray_caster.history_idx - 1 + kRayHistoryLength) %
+            kRayHistoryLength;
         for (int i = 0; i < kNumEnemies; ++i) {
 
           Vector2D dir_to_player =
               (scene.player.position_ - scene.enemy.position[i]).Normalized();
           float velocity_towards_player =
               scene.enemy.velocity[i].Dot(dir_to_player);
-          value_array[i] = velocity_towards_player;
+
+          bool is_player_detected =
+              IsEntityTypePresent(scene.enemy.ray_caster.ray_hit_types,
+                                  current_history_idx, i, EntityType::player);
+          value_array[i] = velocity_towards_player * is_player_detected;
         }
 
         return value_array;

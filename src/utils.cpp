3fee0575c9af298a48d85020f3debfe6f04cab57
madyhr <marcusdyhr@gmail.com>
2026-03-31 11:55:01 +0200

    Update type casting in utils of the get avg frame time func

--- src/utils.cpp
+++ src/utils.cpp
@@ -20,7 +20,7 @@ float FrameStats::get_average_frame_time() {
   if (current_buffer_length == 0) {
     return 0.0f;
   }
-  return static_cast<float>(frame_time_sum / current_buffer_length);
+  return frame_time_sum / static_cast<float>(current_buffer_length);
 };
 
 void FrameStats::print_fps_running_average(float dt) {
